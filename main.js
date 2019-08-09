25d4123bd012127be8d0e513c9fa35ef149d0b66
Martin Novotny <novotny.martin253@gmail.com>
2019-08-09 23:09:28 +0200

    nacitanie json dat z API

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
+var dataURL = 'https://jsonstorage.net/api/items/aa5ea59c-da25-4c76-a8ad-c8f2cd825b27';
+
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Hello, World!'
+        urlData: []
+    },
+    mounted() {
+        var self = this
+        $.getJSON(dataURL, function(data) {
+            console.log(arguments);
+            self.urlData = data;
+        });
     }
 })
