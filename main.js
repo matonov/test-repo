920acdbc0b08242f57f3928ce14f4ba8b65e016b
Martin Novotny <novotny.martin253@gmail.com>
2019-08-10 16:30:56 +0200

    implementovane nove funkcie

--- main.js
+++ main.js
@@ -3,13 +3,44 @@ var dataURL = 'https://jsonstorage.net/api/items/aa5ea59c-da25-4c76-a8ad-c8f2cd8
 var app = new Vue({
     el: '#app',
     data: {
-        urlData: []
+        urlData: [],
+        cart: 0
     },
     mounted() {
         var self = this
         $.getJSON(dataURL, function(data) {
             console.log(arguments);
             self.urlData = data;
-        });
+        })
+    },
+    computed: {
+        price() {
+            return getPrice(this.urlData.Price, this.urlData.CurrencyCode);
+        },
+        oldPrice() {
+            return getPrice(this.urlData.OldPrice, this.urlData.CurrencyCode);
+        },
+        discount() {
+            return getDiscount(this.urlData.Discount);
+        },
+        colorName() {
+            return 'červená';
+        }
     }
-})
+});
+
+function getPrice(priceValue, currencyCode) {
+    if (priceValue == null && currencyCode == null) {
+        return '';
+    }
+
+    return priceValue + (currencyCode === 'EUR' ? '€': currencyCode);
+}
+
+function getDiscount(discount) {
+    if (discount == null) {
+        return '';
+    }
+
+    return discount + ' zľava';
+}
